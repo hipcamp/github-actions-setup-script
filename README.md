09ad00767105376e9c21ae7a441c9de31a3d35cd
Caleb Gosiak <caleb@hipcamp.com>
2021-09-15 10:48:44 -0500

    setup script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # github-actions-setup-script
 This is a utility script for setting up GitHub Actions Runners.
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/hipcamp/github-actions-setup-script/main/setup.sh)" RUNNERS=[number of runners] GITHUB_URL=[https://github.com/your-org] TOKEN=[your token] LABELS=[label_1,label_2]
---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+for ARGUMENT in "$@"
+do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
+    case "$KEY" in
+            RUNNERS)              RUNNERS=${VALUE} ;;
+            GITHUB_URL)           GITHUB_URL=${VALUE} ;;
+            TOKEN)                TOKEN=${VALUE} ;;
+            LABELS)               LABELS=${VALUE} ;;
+            *)   
+    esac    
+done
+
+## ROOTLESS DOCKER Runner Setup
+sudo add-apt-repository ppa:git-core/ppa -y
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install git -y
+sudo apt install -y nodejs
+sudo apt install -y uidmap
+sudo apt install -y dbus-user-session
+sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo npm i -g yarn
+sudo apt upgrade -y
+dockerd-rootless-setuptool.sh install
+# Add output to ~/.bashrc
+echo '# Docker' >> ~/.bashrc
+echo "export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock" >> ~/.bashrc
+echo 'export PATH=/usr/bin:$PATH' >> ~/.bashrc
+# ===
+systemctl --user start docker
+systemctl --user enable docker
+sudo loginctl enable-linger $(whoami)
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+
+for i in $(seq 1 "$RUNNERS") 
+do
+    echo "Setting up new runner: $(hostname)-$i"
+    mkdir actions-runner-$i && cd actions-runner-$i
+    curl -o actions-runner-linux-x64-2.281.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.281.1/actions-runner-linux-x64-2.281.1.tar.gz
+    tar xzf ./actions-runner-linux-x64-2.281.1.tar.gz
+    sudo ./bin/installdependencies.sh
+    ./config.sh --unattended --name $(hostname)-$i --url $GITHUB_URL --token $TOKEN --labels $LABELS
+    sudo ./svc.sh install
+    sudo ./svc.sh start
+    cd ~
+done
+
+sudo reboot
