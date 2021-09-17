f4476b9c23eb2a3beae2463714994a4d5316f805
Caleb Gosiak <caleb@hipcamp.com>
2021-09-17 15:13:19 -0500

    additional tools

--- setup.sh
+++ setup.sh
@@ -19,17 +19,27 @@ echo "Setting up $RUNNERS Runners"
 sudo add-apt-repository ppa:git-core/ppa -y
 curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update -y
-sudo apt install git -y
-sudo apt install -y nodejs
-sudo apt install -y uidmap
-sudo apt install -y dbus-user-session
-sudo apt install -y apt-transport-https ca-certificates curl gnupg lsb-release
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo apt install -y build-essential
+sudo apt-get update -y
+sudo apt-get install git -y
+sudo apt-get install -y nodejs
+sudo apt-get install -y uidmap
+sudo apt-get install -y dbus-user-session
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y build-essential
+sudo apt-get install -y unzip
+sudo apt-get install -y rubygems
 sudo npm i -g yarn
-sudo apt upgrade -y
+sudo apt-get upgrade -y
+wget --quiet https://github.com/mozilla/sops/releases/download/v3.7.1/sops_3.7.1_amd64.deb
+sudo dpkg -i sops_3.7.1_amd64.deb
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 # Add output to ~/.bashrc
 echo '# Docker' >> ~/.bashrc
 echo "export DOCKER_HOST=unix:///run/user/$(id -u)/docker.sock" >> ~/.bashrc
@@ -45,6 +55,8 @@ systemctl --user enable docker
 sudo loginctl enable-linger $(whoami)
 sudo groupadd docker
 sudo usermod -aG docker ${USER}
+# Docker System Prune Daily
+(crontab -u $(whoami) -l; echo "0 0 * * * /usr/bin/docker system prune -f --all" ) | crontab -u $(whoami) -
 
 for i in $(seq 1 "$RUNNERS") 
 do
